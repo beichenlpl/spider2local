5bbbfa5bbadc71052373397997b7a6994782c679
北晨 <beichenlpl@foxmail.com>
2023-11-02 06:29:43 +0000

    update demo/main.py.
    Signed-off-by: 北晨 <beichenlpl@foxmail.com>

--- demo/main.py
+++ demo/main.py
@@ -1,33 +1,8 @@
-from spider2local import send_request, resp_filter, Request, ResponseType, Filter, FilterRule
-from time import sleep
+from spider2local import send_request, resp_filter, Request, Filter, FilterRule
 
 if __name__ == '__main__':
-    req = Request(
-        host='https://xueqiu.com/',
-        uri='statuses/hot/listV2.json',
-        params={
-            'since_id': -1,
-            'max_id': -1,
-            'size': 15
-        },
-        resp_type=ResponseType.JSON
-    )
-    req.auto_set_cookie()
+    req = Request('https://gitee.com/')
     resp = send_request(req)
-
-    data = resp.get_content()
-    for item in data['items']:
-        user_id = item['original_status']['user_id']
-        article_id = item['original_status']['id']
-        req = Request(
-            host='https://xueqiu.com/',
-            uri=f'{user_id}/{article_id}'
-        )
-        req.auto_set_cookie()
-        resp = send_request(req)
-
-        filter_rules = [FilterRule('div', {'class': 'article__bd__detail'})]
-        entity = resp_filter(Filter(resp, filter_rules))
-        print(entity.text())
-
-        sleep(10)
+    rules = [FilterRule('title', {})]
+    entity = resp_filter(Filter(resp, rules))
+    print(entity.text())
