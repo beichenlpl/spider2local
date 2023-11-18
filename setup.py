e033f46d6f22567f3a9792dc13ece54b7763a49d
北晨 <beichenlpl@foxmail.com>
2023-11-18 13:14:36 +0800

    添加0.0.1版本描述

--- setup.py
+++ setup.py
@@ -2,8 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='spider2local',
+    author_email='beichenlpl@foxmail.com',
     version='0.1.1',
     packages=find_packages(),
+    url='https://github.com/beichenlpl/spider2local',
     install_requires=[
         'requests',
         'bs4',
